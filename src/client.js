e6bbeaac773b83d0075031bf615ae6c20d68a9c5
Blair Young <blairyoung009@gmail.com>
2022-08-04 14:32:16 -0700

    working on contentful hooks

--- src/client.js
+++ src/client.js
@@ -1,6 +1,23 @@
-import * as contentful from 'contentful';
+import {createClient} from "contentful";
 
-export const client = contentful.createClient({
-    space : process.env.REACT_APP_SPACE_ID,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN
-})
+export const client = () => {
+    createClient({
+        space: process.env.REACT_APP_SPACE_ID ,
+        accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+        host: "preview.contentful.com"
+    });
+};
+
+const getRecipes = async () => {
+    try{
+        await client.getEntries({
+            content_type: "recipe",
+            select: "fields"
+        });
+        return entries;
+    } catch(error){
+        console.log(`error fetch: ${error}`);
+    }
+
+    return {getRecipes};
+}
