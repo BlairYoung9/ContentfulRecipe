1a29903da7c8f5aea8b08c7b108ff8b52e1e5448
Blair Young <blairyoung009@gmail.com>
2022-08-07 15:03:02 -0700

    added sass

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useCallback} from 'react'
 import { client } from '../../client'
+import CarouselSlide from './carouselSlide'
 
 const Carousel = () => {
 
@@ -10,7 +11,7 @@ const Carousel = () => {
         const cleanSlides = rawData.map((slide) => {
             const {sys,fields} = slide;
             const {id} = sys;
-            const slideTitle = fields.slide;
+            const slideTitle = fields.title;
             const slideDescription = fields.description;
             const slideBg = fields.image.fields.file.url;
             const updatedSlide = {id, slideTitle, slideDescription, slideBg}
@@ -20,18 +21,20 @@ const Carousel = () => {
     }, [])
 
     const getCarouselSlides =  useCallback(async () => {
+        setIsCarouselLoading(true)
         try {
             const res = await client().getEntries({content_type: 'kitchenCarousel'})
             const resData = res.items
-            console.log(resData)
 
             if(resData){
                 cleanUpCarouselSlides(resData)
             }else{
                 setCarouselSlides([])
             }
+            setIsCarouselLoading(false)
         }catch(err){
             console.log(err)
+            setIsCarouselLoading(false)
         }
     }, [cleanUpCarouselSlides])
 
@@ -39,9 +42,18 @@ const Carousel = () => {
         getCarouselSlides()
     },[getCarouselSlides])
 
+    console.log(carouselSlides)
+
     return (
         <div>
-            <h1>Kitchen Pics</h1>
+            {carouselSlides.map((item) => {
+                const {id, slideBg, slideTitle, slideDescription} = item
+                return(
+                <CarouselSlide key = {id} slideTitle = {slideTitle} slideDescription = {slideDescription} slideBg = {slideBg} />
+                )
+                    
+                
+            })}
         </div>
     )
 }
