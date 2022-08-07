19bc8c41763a2284770636b1430a814b347da92a
Blair Young <blairyoung009@gmail.com>
2022-08-07 14:08:33 -0700

    working on carousel

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallBack} from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import { client } from '../../client'
 
 const Carousel = () => {
@@ -6,16 +6,27 @@ const Carousel = () => {
     const [isCarouselLoading, setIsCarouselLoading] = useState(false)
     const [carouselSlides, setCarouselSlides] = useState([])
 
-    const getCarouselSlides =  useCallBack(async () => {
+    const cleanUpCarouselSlides = (rawData) => {
+        const cleanSlides = rawData.map((slide) => {
+            const {sys,fields} = slide;
+            const {id} = sys;
+            const slideTitle = fields.slide;
+            const slideDescription = fields.description;
+            const slideBg = fields.image.fields.file.url;
+            const updatedSlide = {id, slideTitle, slideDescription,. slideBg}
+        })
+    }
+
+    const getCarouselSlides =  useCallback(async () => {
         try {
-            const res = await client.getEntries({content_type: 'kitchenCarousel'})
+            const res = await client().getEntries({content_type: 'kitchenCarousel'})
             const resData = res.items
             console.log(resData)
             console.log(res)
         }catch(err){
             console.log(err)
         }
-    })
+    }, [])
 
     useEffect(() => {
         getCarouselSlides()
