c605df43bf61279b2fe12aa64d1fb899e1457c6f
Blair Young <blairyoung009@gmail.com>
2022-08-09 15:42:47 -0700

    deployment commit

--- src/components/recipes/RecipeCard.js
+++ src/components/recipes/RecipeCard.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const RecipeCard = (props) => {
     
-    const {id, recipeTitle, recipeDirections, recipeBg, recipeIngredients} = props;
+    const {id, recipeName, recipeDirections, recipeBg, recipeIngredients} = props;
     return (
         <div className="slideWrap" style={{ backgroundImage: `url(${recipeBg})` }}>
             <div className='textWrap'>
-                <h2>{recipeTitle}</h2>
+                <h2>{recipeName}</h2>
+                <h1>Directions</h1>
                 <p>{recipeDirections}</p>
+                <h1>Ingredients</h1>
                 <p>{recipeIngredients}</p>
-                <a href='/recipes' className='btn'>Learn More</a>
+                <a href='/' className='btn'>Home</a>
             </div>
         </div>
     )
