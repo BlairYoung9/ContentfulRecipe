aec98939bc820de09588567f5d46ac3db68c3e39
Blair Young <blairyoung009@gmail.com>
2022-08-09 15:42:23 -0700

    deployment commit

--- src/client.js
+++ src/client.js
@@ -2,34 +2,9 @@ import { createClient } from "contentful";
 
 
 export const client = () => {
-    //console.log(process.env)
     let whatever = createClient({
         space: process.env.REACT_APP_SPACE_ID,
         accessToken: process.env.REACT_APP_ACCESS_TOKEN,
     });
     return whatever;
 };
-
-
-// export const getRecipes = async () => {
-//     try {
-//         const entries = await client().getEntries({
-//             content_type: "recipe",
-//             select: "fields"
-//         });
-
-//         const sanitizedEntries = entries.items.map((item) => {
-//             const photo = item.fields.photo.fields;
-//             return {
-//                 ...item.fields,
-//                 photo
-//             };
-//         });
-        
-//         return sanitizedEntries;
-
-//     } catch (error) {
-//         console.log(`error fetch: ${error}`);
-//         return;
-//     }
-// }
