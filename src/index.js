34d1e814983b6f4f07b07fac3c4eaee931b32495
Blair Young <blairyoung009@gmail.com>
2022-11-12 13:23:52 -0800

    added _redirects

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.scss';
 import App from './App';
-import { Link, Switch, Route, BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
